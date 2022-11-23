7c1a4bd7f1c63ef776b67b5fa2108dde6aa4f54b
kmg0485 <kmg0485@naver.com>
2022-11-23 13:08:07 +0900

    :sparkles: feat : image_download #9

--- painters/urls.py
+++ painters/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
 from painters import views
 
-urlpatterns = []
+urlpatterns = [
+        # path('download/<int:pk>', views.Download_view, name="download"),
+]
--- painters/views.py
+++ painters/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+import mimetypes
+import os
+from django.http import HttpResponse, Http404
+import urllib
+from django.conf import settings
+from .models import Painter, Painting
 
-# Create your views here.
+"""
+def Download_view(request, pk):
+    painting = get_object_or_404(Painting, pk=pk)
+    url = painting.image.url[1:]
+    file_url = urllib.parse.unquote(url)
+    
+    if os.path.exists(file_url):
+        with open(file_url, 'rb') as fh:
+            # quote_file_url = urllib.parse.quote(painting.filename.encode('utf-8'))
+            response = HttpResponse(fh.read(), content_type=mimetypes.guess_type(file_url)[0])
+            # response['Content-Disposition'] = 'attachment;filename*=UTF-8\'\'%s' % quote_file_url
+            return response
+        raise Http404
+"""    
+    
